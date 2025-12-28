56a17050430b8ac8702f9590720189b5046986a1
mariagarciacorreas <mariagc094@gmail.com>
2025-12-28 13:06:07 +0100

    Add PrimarIA_sanitycode project

---
+++ PrimarIA_sanitycode/back/src/main/java/es/riberadeltajo/primaria_sanitycode/PrimarIaSanitycodeApplication.java
@@ -0,0 +1,13 @@
+package es.riberadeltajo.primaria_sanitycode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimarIaSanitycodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrimarIaSanitycodeApplication.class, args);
+    }
+
+}
---
+++ PrimarIA_sanitycode/back/src/test/java/es/riberadeltajo/primaria_sanitycode/PrimarIaSanitycodeApplicationTests.java
@@ -0,0 +1,13 @@
+package es.riberadeltajo.primaria_sanitycode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimarIaSanitycodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
